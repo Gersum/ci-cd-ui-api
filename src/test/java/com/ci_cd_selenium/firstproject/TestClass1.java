c17b7897d721466dc02ba5dfffabc64d44f3bdc4
gersum <gersumasfaw2010@gmail.com>
2023-07-03 16:01:18 +0300

    new commit

--- src/test/java/com/ci_cd_selenium/firstproject/TestClass1.java
+++ src/test/java/com/ci_cd_selenium/firstproject/TestClass1.java
@@ -3,9 +3,9 @@ package com.ci_cd_selenium.firstproject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 
 public class TestClass1 {
 	 WebDriver driver;
